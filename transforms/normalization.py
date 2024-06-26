78e071467e146fb5a732aa30503be8273cff7032
Anders Bredgaard Thuesen <anders@teton.ai>
2024-06-26 16:41:45 +0200

    Fix normalization

--- transforms/normalization.py
+++ transforms/normalization.py
@@ -1,21 +1,31 @@
 import torch
 
+EPSILON = 1e-6
+
 
 class Normalize:
-    def __init__(self, mean: torch.Tensor, std: torch.Tensor):
+    def __init__(self, mean: torch.Tensor, std: torch.Tensor, eps=EPSILON):
         self.mean = mean
         self.std = std
+        self.eps = eps
 
     def __call__(self, data):
         x, *rest = data
-        return (x - self.mean) / self.std, *rest
+        if x.isnan().any():
+            raise ValueError("NaNs in input")
+
+        return (x - self.mean) / (self.std + self.eps), *rest
 
 
 class Denormalize:
-    def __init__(self, mean: torch.Tensor, std: torch.Tensor):
+    def __init__(self, mean: torch.Tensor, std: torch.Tensor, eps=EPSILON):
         self.mean = mean
         self.std = std
+        self.eps = eps
 
     def __call__(self, data):
         x, *rest = data
-        return x * self.std + self.mean, *rest
+        if x.isnan().any():
+            raise ValueError("NaNs in input")
+
+        return x * (self.std + self.eps) + self.mean, *rest
