f7d54179409c3c3b3401981aad17aadb3b9bbb6b
Anders Bredgaard Thuesen <anders@teton.ai>
2024-06-26 16:43:16 +0200

    Compute norms

--- compute_norms.py
+++ compute_norms.py
@@ -3,12 +3,18 @@ from datasets import DataModule
 from configs import get_config
 from tqdm import tqdm
 
+import smplx
 
 if __name__ == "__main__":
+    model_cfg = get_config("configs/model.yaml")
     train_cfg = get_config("configs/train.yaml")
-    data_cfg = get_config("configs/datasets.yaml").teton
+    data_cfg = get_config("configs/datasets.yaml")
 
-    datamodule = DataModule(data_cfg, 1, train_cfg.TRAIN.NUM_WORKERS)
+    smpl = smplx.SMPLLayer(
+        model_path=model_cfg.SMPL_MODEL_PATH,
+    )
+
+    datamodule = DataModule(data_cfg, 1, train_cfg.TRAIN.NUM_WORKERS, smpl)
 
     # Use this code to calculate mean and std
     datamodule.setup()
@@ -19,24 +25,21 @@ if __name__ == "__main__":
     for motion, mask, *_ in tqdm(
         datamodule.train_dataloader(), desc="Computing mean and std"
     ):
-        pass
+        masked_motion = motion[mask]
+        mean = masked_motion.mean(dim=0)
+        std = masked_motion.std(dim=0)
 
-    exit(0)
-    # masked_motion = motion[mask]
-    # mean = masked_motion.mean(dim=0)
-    # std = masked_motion.std(dim=0)
-
-    # if mean.isnan().any():
-    #     print("mean is nan")
-    # elif std.isnan().any():
-    #     print("std is nan")
-    # else:
-    #     means.append(mean)
-    #     stds.append(std)
+        if mean.isnan().any():
+            print("mean is nan")
+        elif std.isnan().any():
+            print("std is nan")
+        else:
+            means.append(mean)
+            stds.append(std)
 
     mean = torch.stack(means).mean(dim=0)
     std = torch.stack(stds).mean(dim=0)
 
     # Save mean and std
-    torch.save(mean, "means.pt")
-    torch.save(std, "stds.pt")
+    torch.save(mean, "mean.pt")
+    torch.save(std, "std.pt")
